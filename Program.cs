59c425f5ce9e12381c4378e642588111801dc625
Isaque-Gomes <116127894+Isaque-Gomes@users.noreply.github.com>
2023-02-09 12:16:44 -0300

    [M1S01] Ex 9 - Banco (Desafio)
    [Listas, estrutura de repetição, estrutura de decisão ]
    Agora que estão feras no assunto, crie um programa bancário, que contenha duas funcionalidades, adicionar Transação e Consultar Extrato; 
    
    Ao iniciar o programa ele deverá exibir um menu de boas vindas ex:
    Saída: Bem vindo ao Banco Nacional Coqueiros, por favor selecione uma opção para continuar: 
    1.	Adicionar Transação;
    2.	Consultar extrato;
    3.	Sair;
    
    O usuário deve selecionar a opção desejada, caso selecione uma opção inválida, o programa deverá novamente apresentar o menu; 
    
    Caso selecione a opção 1 - Adicionar Transação: o programa deverá ler o valor da transação informada  pelo cliente, e adicionar em uma lista de transações;
    
    Caso selecione a opção 2 - Consultar extrato, deve ser impresso todo o histórico de transações, presente  na lista de transações, e ao final exibir o saldo, que é a somatória de toda a lista de transação (seja ele positivo ou negativo);
    
    Caso selecione a opção 3 - Sair o programa deve ser encerrado;

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿// Instituição bancária Coqueiros
+string opcao;
+string opcao2;
+string opcao3 = "0";
+float insercao;
+
+Console.WriteLine("Olá, bem vindo ao banco nacional Coqueiros! Por favor, selecione uma opção para continuar:");
+do{
+    Console.WriteLine("1. Adicionar uma transação");
+    Console.WriteLine("2. Consultar extrato");
+    Console.WriteLine("3. Sair");
+    opcao = Console.ReadLine();
+} while (!(opcao == "1" || opcao == "2" || opcao == "3"));
+List<float> transacoes = new List<float>();
+if(opcao == "1"){
+    do{
+        Console.WriteLine("Digite o valor da transação:");
+        while(! float.TryParse(Console.ReadLine(), out insercao)){
+            Console.WriteLine("Favor digitar somente números. Digite o valor da transacao:");
+        };
+        transacoes.Add(insercao);
+        Console.WriteLine("O que deseja fazer agora?");
+        Console.WriteLine("1. Adicionar mais uma transação");
+        Console.WriteLine("2. Consultar extrato");
+        Console.WriteLine("3. Sair");
+        opcao2 = Console.ReadLine();
+    } while(!(opcao2 == "1" || opcao2 == "2" || opcao2 == "3"));
+    if(opcao2 == "1"){
+        do{
+        Console.WriteLine("Digite o valor da transação:");
+        while(! float.TryParse(Console.ReadLine(), out insercao)){
+            Console.WriteLine("Favor digitar somente números. Digite o valor da transacao:");
+        };
+        transacoes.Add(insercao);
+        Console.WriteLine("Não é possível mais adicionar transações. O que deseja fazer agora?");
+        Console.WriteLine("1. Consultar extrato");
+        Console.WriteLine("2. Sair");
+        opcao3 = Console.ReadLine();
+    } while(!(opcao3 == "1" || opcao3 == "2"));
+    }
+    if(opcao3 == "1" || opcao == "2" || opcao2 == "2"){
+        Console.WriteLine("Os valores das transações foram:");
+        foreach(var transacao in transacoes){
+            Console.WriteLine(transacao.ToString("N"));
+        }
+
+    } else{
+        Console.WriteLine("Obrigado por sua visita. Até logo!");
+    }
+} else{
+        Console.WriteLine("Obrigado por sua visita. Até logo!");
+}
